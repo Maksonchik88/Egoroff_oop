67f15be133cd6a4712b9168151c075d33d014033
Maksim Kolesnikov <89533464344maks@gmail.com>
2023-03-30 16:30:41 +0300

    class_person

---
+++ class_person.py
@@ -0,0 +1,29 @@
+class Person:
+    def __init__(self, name, surname, age):
+        self.first_name = name
+        self.last_name = surname
+        self.age = age
+
+    def full_name(self):
+        return f"{self.last_name} {self.first_name}"
+
+    def is_adult(self):
+        return True if self.age >= 18 else False
+
+
+p1 = Person('Ash', 'Ketchum', 20)
+assert isinstance(p1, Person)
+assert p1.full_name() == 'Ketchum Ash'
+assert p1.age == 20
+assert p1.first_name == 'Ash'
+assert p1.last_name == 'Ketchum'
+assert p1.is_adult() is True
+
+p2 = Person('Hermione', 'Granger', 16)
+assert isinstance(p2, Person)
+assert p2.age == 16
+assert p2.first_name == 'Hermione'
+assert p2.last_name == 'Granger'
+assert p2.full_name() == 'Granger Hermione'
+assert p2.is_adult() is False
+print('Good')
